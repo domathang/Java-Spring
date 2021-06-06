978f2ade6c2a4c5d382a16acf23533629419f231
kwakdony <kwakdh25@gmail.com>
2021-06-06 23:51:05 +0900

    AOP

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -27,6 +27,7 @@ public class MemberService {
     }
 
     private void validateDuplicateMember(Member member) {
+
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
@@ -37,6 +38,7 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
+
         return memberRepository.findAll();
     }
 
