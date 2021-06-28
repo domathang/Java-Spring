36aef84c7ce6600bc33490c11e5cc047624ef989
kwakdony <kwakdh25@gmail.com>
2021-06-28 22:58:29 +0900

    JPA와 H2에 대한 기본적인 기능과 설정

---
+++ aws-spring-book/src/main/java/com/ouu99/book/springboot/Application.java
@@ -0,0 +1,12 @@
+package com.ouu99.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.
+        SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
