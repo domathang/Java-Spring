55a4360678760f828741d7918abc4016c54e0aba
kwakdony <kwakdh25@gmail.com>
2021-06-29 22:43:51 +0900

    Chapter03/ Spring Data Jpa로 DB 다뤄보기

--- aws-spring-book/src/main/java/com/ouu99/book/springboot/Application.java
+++ aws-spring-book/src/main/java/com/ouu99/book/springboot/Application.java
@@ -3,9 +3,12 @@ package com.ouu99.book.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.
         SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
