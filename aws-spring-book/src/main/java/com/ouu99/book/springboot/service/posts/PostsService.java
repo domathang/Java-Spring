93dcb39848f16556156638f4a244c3e8bb7e8bda
kwakdony <kwakdh25@gmail.com>
2021-06-30 16:46:48 +0900

    Chapter04/ 서버 템플릿 엔진(머스테치)을 사용하여 기본적인 게시물 CRUD의 UI 구현

--- aws-spring-book/src/main/java/com/ouu99/book/springboot/service/posts/PostsService.java
+++ aws-spring-book/src/main/java/com/ouu99/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.ouu99.book.springboot.service.posts;
 
 import com.ouu99.book.springboot.domain.posts.Posts;
 import com.ouu99.book.springboot.domain.posts.PostsRepository;
+import com.ouu99.book.springboot.web.dto.PostsListResponseDto;
 import com.ouu99.book.springboot.web.dto.PostsResponseDto;
 import com.ouu99.book.springboot.web.dto.PostsSaveRequestDto;
 import com.ouu99.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,6 +35,15 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new
+                        IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new
@@ -38,4 +51,12 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findByAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
 }
