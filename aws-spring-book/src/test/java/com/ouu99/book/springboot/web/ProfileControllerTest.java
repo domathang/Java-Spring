f62de431662d3862223e23214d532b9bcd43dd53
kwakdony <kwakdh25@gmail.com>
2021-07-05 14:56:03 +0900

    import문 삭제

--- aws-spring-book/src/test/java/com/ouu99/book/springboot/web/ProfileControllerTest.java
+++ aws-spring-book/src/test/java/com/ouu99/book/springboot/web/ProfileControllerTest.java
@@ -1,6 +1,5 @@
 package com.ouu99.book.springboot.web;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
